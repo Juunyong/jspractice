da583d7b6606adfcf21348d69014dd77a103b60b
Juunyong <qkrwnsdyd212@gmail.com>
2024-10-18 16:07:36 +0900

    1018

--- js/jquery.js
+++ js/jquery.js
@@ -1,9 +1,19 @@
 $(function () {
+    $('.btn1')
+        .on('mouseenter', function () {
+            $('.box1').slideDown(1000);
+        })
+        .on('mouseleave', function () {
+            $('.box1').slideUp(1000);
+        });
+});
+
+/*$(function () {
     $('.btn1').on('click', function () {
         $('.box1').toggle('slow');
     });
 });
-
+/*
 $(function () {
     $('.btn2').mouseenter(function () {
         $('.box2').hide();
@@ -13,5 +23,24 @@ $(function () {
 $(function () {
     $('.btn2').mouseleave(function () {
         $('.box2').show();
+    });
+});*/
+
+$(function () {
+    $('.btn2').mouseenter(function () {
+        $('.box2').slideToggle(5000);
+    });
+});
+
+$(function () {
+    $('.btn3').click(function () {
+        $('.box3').animate(
+            {
+                width: '+=150px',
+                left: 100,
+                top: 100,
+            },
+            1000
+        );
     });
 });
